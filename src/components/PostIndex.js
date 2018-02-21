30c436cf97ff6e625d4813b9f508090548058fcd
Zach Janice <zjanice@imodules.com>
2018-02-21 11:16:23 -0600

    upload local project on redux

---
+++ src/components/PostIndex.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions'
+
+class PostIndex extends Component {
+  render () {
+    return (
+      <div>
+        PostIndex
+      </div>
+    )
+  }
+}
+
+export default connect(null, { fetchPosts })(PostIndex);
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+
+import PostIndex from './components/PostIndex';
+
+import reducers from './reducers/index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={PostIndex} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
